d9ad9b41ee137a2874997e5ae5edc9c86a473478
Nelson Neuberger <nelson.neuberger@gmail.com>
2019-05-29 09:08:31 -0600

    added colors

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ var title = document.getElementById("title");
 title.addEventListener("click", function(){
   debugger
 })
-
 var options = ["rock", "paper", "scissors"];
+var colors = ["yellow", "green", "red"];
+var results = ["You lost!", "You won!"];
 var userChoice;
+
+var btn1 = document.getElementById('btn1');
+btn1.addEventListener("click", changeColor);
+function changeColor() {
+  document.body.style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+};
+
+var btn2 = document.getElementById('btn2');
+btn2.addEventListener("click", changeColor);
+function changeColor() {
+  document.body.style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+};
+
+var btn3 = document.getElementById('btn3');
+btn3.addEventListener("click", changeColor);
+function changeColor() {
+  document.body.style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+};
 
 var choices = document.getElementsByClassName("choice");
 console.log(choices);
@@ -18,6 +37,6 @@ for(var i = 0; i < choices.length; i++) {
     userChoice = this.id;
     // get the comp choice
     // display choices
-    // determine winer 
+    // determine winner 
   });
 }
